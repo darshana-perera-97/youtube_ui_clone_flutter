bb47a5d98cf4407dfb9f37886eb035e70226f20c
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-16 15:27:11 +0530

    top bar created

--- lib/mobileBody.dart
+++ lib/mobileBody.dart
@@ -6,8 +6,38 @@ class mobileBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(title: Text("as")),
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Image.asset(
+            "assets/logo.png",
+            height: 100.0,
+            width: 100.0,
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.video_call,
+                color: Colors.black54,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.black87,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.black87,
+              )),
+        ],
+      ),
     );
   }
 }
